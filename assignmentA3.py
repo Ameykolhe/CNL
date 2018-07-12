c9ca51975396a8790430e4043d84c5133ccf4aad
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-07-12 23:35:03 +0530

    completed subnetting and ping

--- assignmentA3.py
+++ assignmentA3.py
@@ -3,6 +3,8 @@ import math
 import argparse
 from subprocess import Popen,PIPE
 import platform
+import ipaddress
+import os
 
 def pingIP(args):
 
@@ -14,15 +16,15 @@ def pingIP(args):
     exitCode = proc.returncode
     out = out.decode('utf-8')
     err = err.decode('utf-8')
-    s = "Ping unsuccessful : "
-    if exitCode == 0:
-        print("PING Successful")
-    elif "Destination Host Unreachable" in out:
+    s = "\nPing unsuccessful : "
+    if exitCode != 0:
+        print(s)
+    elif ("Destination Host Unreachable" in out )  or ('Destination host unreachable' in out) :
         print(s + "Destination Host Unreachable")
     elif "Request Timed Out" in out:
         print(s + "Request Timed Out") 
     else:
-        print(s)
+        print("\nPING Successful")
         
 
 
@@ -37,7 +39,7 @@ def cidrSubnetMask(ip,n):
         submask[0] = 255
         submask[1] = 255
         submask[2] = sum([2**( 7 - x ) for x in range(n - 16) ])
-    elif n in range(24,32):
+    elif n in range(24,33):
         submask[0] = 255
         submask[1] = 255
         submask[2] = 255
@@ -45,59 +47,140 @@ def cidrSubnetMask(ip,n):
     return submask
 
 
+def printDetail(*arg):
+    if arg[0] != None:
+        print("Class : " ,  arg[0])
+    print("IP : " , arg[1] , type(arg[1]))
+    print("NetId : " ,  arg[2])
+    print("SubnetMask : ",arg[3], type(arg[3]) ,'\n')
+    if arg[5] == True:
+        #print(type(arg[4][0]))
+        network = arg[4]
+        ip = ipaddress.IPv4Address(str(arg[1]).split('/')[0])
+        sno = 0
+        fl = None
+        print("SunbetNo\tStartAddress\t\tLastAddress")
+        for sunbnetAddresses in network:
+            sno += 1
+            sunbnetAddresses = list(sunbnetAddresses)
+            print("{}\t\t{}\t\t{}".format(sno,str(sunbnetAddresses[0]),str(sunbnetAddresses[-1])))
+            if ip in sunbnetAddresses :
+                fl = sno
+        if fl != None :
+            print('\n','Address : ',str(ip) ,'is present in Subnet no. : ' , fl,'\n')
+    return arg
+        
 
 
 def findSubnetMask(args):    
     i = str(args.ip).split('/')
     ip = i[0].split('.')
     ip = [int(x) for x in ip]
+    netId = [0,0,0,0]
 
-    if len(ip) > 4:
-        print('Invalid ip address')
+    subnetting = True if args.s != None else False
+
+    n = 0
+    if args.s == None:
+        args.s = 1 
+    try:
+        n = math.ceil(math.log2(args.s))
+    except ValueError:
+        print('\nNo. of Subnets cannot be 0')
         return
 
-    if('/' in args.ip and args.s == None):
-        print('CIDR')
-        print( 'Subnet Mask = ' , cidrSubnetMask(ip,int(i[1])) )
+    if len(ip) > 4:
+        print('\nInvalid ip address')
+        return
+
+    if('/' in args.ip):
+        if args.s > 2**(32 - int(i[1])) :
+            print("\nInvalid no. of Subnets")
+            return
+        print('\nClassless')
+        l = cidrSubnetMask(ip,int(i[1]))
+        l = '.'.join(str(x) for x in l)
+        ip = '.'.join(str(x) for x in ip)
+        ip = ip + '/' + str(i[1])
+        ip = ipaddress.ip_interface(ip)
+        netId = ip.network
+        if args.s != None:
+            network = list(netId.subnets(n))
+        return printDetail(None,ip,netId,l,network,subnetting)
 
 
     elif('/' not in args.ip):
-        print('Classfull')
-        cl = ""
-        n = 0
-        if args.s == None:
-            args.s = 1 
-        try:
-            n = math.ceil(math.log2(args.s))
-        except ValueError:
-            print('No. of Subnets cannot be 0')
-            return
-            
+        print('\nClassfull')
+        cl = ""   
         
         if ip[0] in range(1,128):
             cl = 'A'
+            netId[0] = ip[0]
             if args.s < 2**24 :
                 l = cidrSubnetMask(ip,n+8)
-                print( 'Class = ' , cl , 'Subnet mask = ' , l)
+                l = '.'.join(str(x) for x in l)
+                ip = '.'.join(str(x) for x in ip)
+                netId = '.'.join(str(x) for x in netId)
+                netId = netId + '/' + str(8)
+                netId = ipaddress.IPv4Network(netId)
+                network = list(netId.subnets(n))
+                return printDetail(cl,ip,netId,l,network,subnetting)
             else:
-                print('Invalid No. of Subnets')
+                print('\nInvalid No. of Subnets')
         
         elif ip[0] in range(128,192):
             cl = 'B'
+            netId[0] = ip[0]
+            netId[1] = ip[1]
             if args.s < 2**16 :
-                print( 'Class = ' , cl , 'Subnet mask = ' ,cidrSubnetMask(ip,n+16) ),
+                l = cidrSubnetMask(ip,n+16)
+                l = '.'.join(str(x) for x in l)
+                ip = '.'.join(str(x) for x in ip)
+                netId = '.'.join(str(x) for x in netId)
+                netId = netId + '/' + str(16)
+                netId = ipaddress.IPv4Network(netId)
+                network = list(netId.subnets(n))
+                return printDetail(cl,ip,netId,l,network,subnetting)
             else:
-                print('Invalid No. of Subnets')
+                print('\nInvalid No. of Subnets')
             
         elif ip[0] in range(192,224):
             cl = 'C'
+            netId[0] = ip[0]
+            netId[1] = ip[1]
+            netId[2] = ip[2]
             if args.s < 2**8 :
-                print( 'Class = ' , cl , 'Subnet mask = ' ,cidrSubnetMask(ip,n+24) ),
+                l = cidrSubnetMask(ip,n+24)
+                l = '.'.join(str(x) for x in l)
+                ip = '.'.join(str(x) for x in ip)
+                netId = '.'.join(str(x) for x in netId)
+                netId = netId + '/' + str(24)
+                netId = ipaddress.IPv4Network(netId)
+                network = list(netId.subnets(n))
+                return printDetail(cl,ip,netId,l,network,subnetting)
             else:
-                print('Invalid No. of Subnets')
+                print('\nInvalid No. of Subnets')
 
     else:
-        print('Invalid Format')
+        print('\nInvalid Format')
+
+
+
+def setIP(args):
+
+    tparser = argparse.ArgumentParser()
+    tparser.add_argument('-ip', type = str , help = 'ip' , required = True )
+    tparser.add_argument('-s' , type = int , default = None , help = 'no of Subnets')
+    temp = parser.parse_args( ['findSubnetMask','-ip',args.ip,'-s',str(args.s)])
+
+    tup = temp.func(temp)
+    try:
+        os.system("sudo ifconfig" + args.interface + 'down')
+        os.system("sudo ifconfig" + args.interface + tup[1] + ' netmask ' , tup[3] )
+        os.system("sudo ifconfig" + args.interface + 'up')
+        print("IP address Updated Successfully")
+    except Exception as e:
+        print(e)
 
 
 
@@ -113,5 +196,12 @@ if __name__ == '__main__' :
     sNetParser.add_argument('-ip', type = str  , help = 'ip' , required = True)
     sNetParser.add_argument('-s' , type = int , default = None , help = 'no of Subnets')
     sNetParser.set_defaults(func = findSubnetMask)
+    
+    setIPParser = subParser.add_parser('setIP')
+    setIPParser.add_argument('-ip', type = str  , help = 'ip' , required = True)
+    setIPParser.add_argument('-s' , type = int , default = None , help = 'no of Subnets')
+    setIPParser.add_argument('-interface',type = str , help = 'Interface Name' , required = True)
+    setIPParser.set_defaults(func = setIP)
+
     args = parser.parse_args()
     args.func(args)
