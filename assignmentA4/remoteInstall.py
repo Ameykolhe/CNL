c09977edb2beb40b3526726225991784862b4a92
Ameykolhe <kolheamey99@gmail.com>
2018-08-24 00:13:59 +0530

    assignmentA4Complete

---
+++ assignmentA4/remoteInstall.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('ifconfig')
+interfaceName = input('Enter Interface Name : ')
+os.system('arp-scan --interface '+ interfaceName + ' --localnet' )
+ipaddress = input('Enter ip address of machine : ')
+
+user = input('Enter User :')
+password = input('password : ')
+
+os.system('sshpass -p {} scp -o StrictHostKeyChecking=no install.py {}@{}:/home/{}'.format(password,user,ipaddress,user))
+
+os.system('sshpass -p {} scp -o StrictHostKeyChecking=no nasm-2.13.03-0.fc24.x86_64.rpm {}@{}:/home/{}'.format(password,user,ipaddress,user))
+
+os.system('sshpass -p {} ssh -t {}@{} python3 install.py'.format(password,user,ipaddress))
