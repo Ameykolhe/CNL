560cc520cca0f2538dc808daaf248a87da266aa6
Ameykolhe <kolheamey99@gmail.com>
2018-08-24 10:06:33 +0530

    .

--- assignmentA4/install.py
+++ assignmentA4/install.py
@@ -2,5 +2,5 @@ import os
 
 sudoPasswd = '1379'
 os.system('echo ' + sudoPasswd + ' | sudo -S apt-get install nasm')
-os.system('rm install.py')
+#os.system('rm install.py')
 os.system('rm nasm-2.13.03-0.fc24.x86_64.rpm')
