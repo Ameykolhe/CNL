7f584d3f1e9dae06d7f708e06269a098b0408283
Amey Kolhe <kolheamey99@gmail.com>
2018-09-20 21:16:35 +0530

    Client Code

--- assignmentA5/client.cpp
+++ assignmentA5/client.cpp
@@ -1,10 +1,7 @@
-#include<stdio.h>
-#include<iostream>
-#include<fstream>
+#include<bits/stdc++.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<errno.h>
-#include<string.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -18,13 +15,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	int sockfd;
-	char buffer[MAXLINE];
+
 	struct sockaddr_in servaddr;
-	
-	ifstream infile;
-	char hello[MAXLINE];
-	infile.open("file.txt");
-	infile.seekg(0);
 	
 	if( (sockfd = socket(AF_INET, SOCK_DGRAM, 0))<0 )
 	{
@@ -34,16 +26,30 @@ int main(int argc, char *argv[])
 	
 	servaddr.sin_family = AF_INET;
    	servaddr.sin_port = htons(LPORT);
-    servaddr.sin_addr.s_addr = inet_addr("10.10.14.173");
-    memset(servaddr.sin_zero, '\0', sizeof(servaddr.sin_zero));  
-    	
-   	int n,len;
-     
-    infile.read(buffer, 45);
-   	sendto(sockfd, (const char *)buffer, strlen(buffer),
-                       0, (const struct sockaddr *) &servaddr, 
-                       sizeof(servaddr));
+    servaddr.sin_addr.s_addr = inet_addr("192.168.0.103");
+    memset(servaddr.sin_zero, '\0', sizeof(servaddr.sin_zero));
+
+
+	string fileName;
+	int len;
+
+	char  *buffer;
+
+	cout<<"Enter File Name : ";
+	cin>>fileName;
+
+	ifstream infile;
+	infile.open( fileName , ios::ate | ios::binary );
+	int fileSize = infile.tellg();
+	infile.seekg(0);
+	buffer = new char[fileSize];
+    infile.read(buffer, fileSize);
+
+	len = sendto(sockfd, (const char *)fileName.c_str() , fileName.length() , 0 , (const struct sockaddr *) &servaddr, sizeof(servaddr));
+   	len = sendto(sockfd, (const char *)buffer, fileSize , 0 , (const struct sockaddr *) &servaddr, sizeof(servaddr));
     
+	cout<<"File Sent"<<endl;
+
     close(sockfd);
     infile.close();
 	return 0;
