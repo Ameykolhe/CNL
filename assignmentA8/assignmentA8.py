b930c69a6ca633334aacd208fb3946541235d873
AmeyKolhe <kolheamey99@gmail.com>
2018-10-10 04:47:49 -0700

    added nslookup command

--- assignmentA8/assignmentA8.py
+++ assignmentA8/assignmentA8.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 def name_to_addr():
     try:
@@ -13,12 +14,22 @@ def addr_to_name():
         print(e)
 
 
+def nslookup():
+    try:
+        string = input('Enter URL / IP address : ')
+        os.system('nslookup ' + string)
+    except Exception as e:
+        print(e)
+
+
 if __name__ == '__main__' :
     while True:
-        choice = input('\n\n\nEnter choice\n1 : Name to Address\n2 : Address to Name\n3 : Exit\n> ')
+        choice = input('\n\n\nEnter choice\n1 : Name to Address\n2 : Address to Name\n3 : NSLOOKUP\n4 : Exit\n> ')
         if choice == '1':
             name_to_addr()
         elif choice == '2':
             addr_to_name()
+        elif choice == '3':
+            nslookup()
         else:
             break
